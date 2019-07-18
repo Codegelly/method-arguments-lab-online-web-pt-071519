d0011e78b03b28bbb3568fa8e3438de8ad003dbb
Angelica  Thompson <i.thompson89@outlook.com>
2019-07-18 01:51:17 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,6 +3,8 @@ def introduction (name)
 end 
 
 def introduction_with_language(arg1, arg2)
+  arg1 = language
+  arg2 = name
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
